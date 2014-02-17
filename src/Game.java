97e924bfd726dcb5dea796b9c4c1bbd6c53342bd
Sid B <banzai.master@gmail.com>
2014-02-17 14:24:36 -0500

    added Game parametrized constructor

--- src/Game.java
+++ src/Game.java
@@ -23,7 +23,48 @@ public class Game {
 	 */
 	public Game(String input) {
 		
-		
+		char[] char_array = input.toCharArray();
+		for(int i = 0; i < char_array.length; i++)
+		{
+			int row = i/5;
+			int col = i%5;
+			
+			Tokens token = null;
+			switch (char_array[i])
+			{
+			case 'e':
+				token = Tokens.EMPTY;
+				emptyRow = row;
+				emptyCol = col;
+				break;
+				
+			case 'r':
+				token = Tokens.RED;
+				break;
+				
+			case 'b':
+				token = Tokens.BLUE;
+				break;
+				
+			case 'w':
+				token = Tokens.WHITE;
+				break;
+				
+			case 'y':
+				token = Tokens.YELLOW;
+				break;
+				
+			case 'g':
+				token = Tokens.GREEN;
+				break;
+				
+			case 'p':
+				token = Tokens.PINK;
+				break;
+			}
+			
+			board[row][col] = token;
+		}
 	}
 	
 	/**
